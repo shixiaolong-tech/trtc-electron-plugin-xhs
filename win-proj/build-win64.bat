45d7d4a56cf31d2062616c2f75f47b5cde7c84ad
Shi Xiaolong <xiaolong1217@yeah.net>
2022-12-30 16:33:26 +0800

    feat: 打包时自动复制带上美颜资源文件

--- win-proj/build-win64.bat
+++ win-proj/build-win64.bat
@@ -17,6 +17,20 @@ del release-win64.zip
 
 mkdir dist-temp
 copy /Y x64\Release\* dist-temp\*
+
+echo "--------------------------------------copy beauty resources"
+mkdir dist-temp\face_beauty.xhs
+mkdir dist-temp\face_beauty.xhs\Beauty_Res
+mkdir dist-temp\face_beauty.xhs\Lut_Res
+mkdir dist-temp\face_beauty.xhs\vsco1
+mkdir dist-temp\face_beauty.xhs\vsco2
+mkdir dist-temp\face_beauty.xhs\vsco3
+xcopy /s ..\.resources\beautify\face_beauty.xhs\Beauty_Res\* dist-temp\face_beauty.xhs\Beauty_Res\*
+xcopy /s ..\.resources\beautify\face_beauty.xhs\Lut_Res\* dist-temp\face_beauty.xhs\Lut_Res\*
+xcopy /s ..\.resources\beautify\face_beauty.xhs\vsco1\* dist-temp\face_beauty.xhs\vsco1\*
+xcopy /s ..\.resources\beautify\face_beauty.xhs\vsco2\* dist-temp\face_beauty.xhs\vsco2\*
+xcopy /s ..\.resources\beautify\face_beauty.xhs\vsco3\* dist-temp\face_beauty.xhs\vsco3\*
+
 cd dist-temp 
 dir
 7z a -tzip ../release-win64.zip ./*
